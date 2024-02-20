72a018c9fdc8aa8fe6db3013515dee03002daf5e
NamasakaLennox <namasakalennox254@gmail.com>
2024-02-20 22:17:53 +0300

    [update]: made a few minor changes

--- frontend/src/Components/SearchResults/SearchResults.jsx
+++ frontend/src/Components/SearchResults/SearchResults.jsx
@@ -1,5 +1,37 @@
 import React from "react";
 
 export const SearchResults = () => {
-  return <div>SearchResults</div>;
+  return (
+    <div>
+      <div className="search-container">
+        <h1>Search For Recipes</h1>
+        <p>Fill at least one field, not all fields are mandatory</p>
+        <div className="search-fields">
+          <div className="search-field-item">
+            <label htmlFor="name">Recipe</label>
+            <input type="text" placeholder="Type food name here" name="name" />
+          </div>
+          <div className="search-field-item">
+            <label htmlFor="name">Cuisine</label>
+            <input type="text" placeholder="Enter text here" name="name" />
+          </div>
+          <div className="search-field-item">
+            <label htmlFor="diet">Diet Preference</label>
+            <select name="diet" id="">
+              <option value="vegan">Vegan</option>
+              <option value="vegetarian">Vegetarian</option>
+              <option value="gluten_free">Gluten Free</option>
+              <option value="ketogenic">Ketogenic</option>
+              {/* more diets to be added */}
+            </select>
+          </div>
+          <div className="search-field-item">
+            <label htmlFor="meal">Meal</label>
+            <input type="text" placeholder="Search By Meal" name="meal" />
+          </div>
+        </div>
+        {/* get random recipe*/}
+      </div>
+    </div>
+  );
 };
--- frontend/src/Pages/Search.jsx
+++ frontend/src/Pages/Search.jsx
@@ -1,26 +1,10 @@
 import React from "react";
+import { SearchResults } from "../Components/SearchResults/SearchResults";
 
 export const Search = () => {
   return (
     <div className="search">
-      <div className="search-container">
-        <h1>Search For Recipes</h1>
-        <div className="search-fields">
-          <div className="search-field-item">
-            <label>Search Recipe</label>
-            <input type="text" placeholder="Enter text here" name="name" />
-          </div>
-          <div className="search-field-item">
-            <label>Diet Preference</label>
-            {/* should be options */}
-          </div>
-          <div className="search-field-item">
-            <label></label>
-            <input type="text" placeholder="Search By Meal" name="name" />
-          </div>
-        </div>
-        {/* get random recipe*/}
-      </div>
+      <SearchResults />
     </div>
   );
 };
